287599331a579ad63a50f73bf47c3bfea08f8d36
Natolin Artem <arnat64@yandex.ru>
2023-02-22 12:37:20 +0300

    added viewsets

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,26 +1,62 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins, viewsets
+
 
 from .serializers import (
     CategorySerializer, CommentSerializer, GenreSerializer,
     ReviewSerializer, TitleSerializer
 )
+from reviews.models import ( 
+    Category, Comment, Genre, Review, Title
+)
 
+# POST, GET(list), DEL методы, остальные запрещены
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericAPIView):
 
-class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = ()
 
-
-class CommentViewSet(ModelViewSet):
+# POST, GET(list, single), DEL, PUT, PUTCH методы, остальные запрещены
+class CommentViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericAPIView):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = ()
 
-
-class GenreViewSet(ModelViewSet):
+# POST, GET(list), DEL методы, остальные запрещены
+class GenreViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericAPIView):
+    queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = ()
 
-
-class ReviewViewSet(ModelViewSet):
+# POST, GET(list, single), DEL, PUT, PUTCH методы, остальные запрещены
+class ReviewViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericAPIView):
+    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = ()
 
-
-class TitleViewSet(ModelViewSet):
+# POST, GET(list, single), DEL, PUT, PUTCH методы, остальные запрещены
+class TitleViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericAPIView):
+    queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    permission_classes = ()
