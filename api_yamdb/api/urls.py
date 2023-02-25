3dca46f978fa67a7a19a4668b024f87d12fcad29
Расим Шаймарданов <shaimardanoff92@yandex.ru>
2023-02-25 23:56:02 +0300

    add auth token

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from users.views import RegistrationViewSet, TokenView
+
 from .views import (
     CategoryViewSet, CommentViewSet, GenreViewSet,
     ReviewViewSet, TitleViewSet
@@ -23,10 +25,9 @@ router.register(
     CommentViewSet,
     basename='comments'
 )
-
+router.register(r'auth/signup', RegistrationViewSet, basename='signup')
+# router.register(r'auth/token', TokenView, basename='token')
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('api/v1/auth/token/',
-         TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
+    path('api/v1/auth/token/', TokenView.as_view()),
 ]
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import User
 
-# тут скоро будет код
+from .serializers import RegistrationSerializer, TokenSerializer
+
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    """
+    Разрешить всем пользователям (аутентифицированным и нет) доступ к данному эндпоинту.
+    """
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+
+
+class TokenView(APIView):
+    """
+    Получить токен с помощью username и confirmation_code.
+    """
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = TokenSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.initial_data.get('username')
+            user = get_object_or_404(User, username=username)
+            confirmation_code = serializer.initial_data.get(
+                'confirmation_code'
+            )
+
+            if user.confirmation_code != confirmation_code:
+                return Response(
+                    {"confirmation_code": "incorrect"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {'token': str(refresh.access_token)},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
