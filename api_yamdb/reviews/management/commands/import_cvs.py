b304282264c0cf00db82b3aa11d6758112a17495
Ivan Tokarev <vans.bass@gmail.com>
2023-03-05 14:43:15 +0300

    Refactoring

--- api_yamdb/reviews/management/commands/import_cvs.py
+++ api_yamdb/reviews/management/commands/import_cvs.py
@@ -6,7 +6,6 @@ from django.core.management.base import BaseCommand
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s, %(levelname)s, %(message)s',
